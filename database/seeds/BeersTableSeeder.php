a13d7de2caba45f54ae1f919d54019e2eafa1f7d
Liutauras <liutauraskav@gmail.com>
2017-08-20 18:58:01 +0300

    Add beer data seeders and data files
    Add csv helper config
    
    Add migration creation from database

---
+++ database/seeds/BeersTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BeersTableSeeder extends Seeder
+{
+    /**
+     * DB table name
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * CSV filename
+     *
+     * @var string
+     */
+    protected $filename;
+
+    public function __construct(){
+
+        $this->table = 'beers';
+        $this->filename = base_path() . '/database/resources/csv/beers.csv';
+
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table($this->table)->delete();
+        $seedData = seedFromCSV($this->filename, ',');
+
+        foreach ($seedData as $t) {
+            if ($t['cat_id'] == '-1') {
+                $t['cat_id'] = null;
+            }
+
+            if ($t['style_id'] == '-1') {
+                $t['style_id'] = null;
+            }
+
+            DB::table($this->table)->insert($t);
+        }
+    }
+}
