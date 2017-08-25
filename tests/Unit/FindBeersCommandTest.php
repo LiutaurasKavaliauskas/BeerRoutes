92d1872a9f881b969128f5069e98545402c4b26e
Liutauras Kavaliauskas <liutauras.k@datadog.org>
2017-08-25 09:53:36 +0300

    Update command unit test

--- tests/Unit/FindBeersCommandTest.php
+++ tests/Unit/FindBeersCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use Exception;
 use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
@@ -22,5 +23,67 @@ class FindBeersCommandTest extends TestCase
         Artisan::call('beers:find');
 
         $this->assertEquals("Not enough parameters\n", Artisan::output());
+    }
+
+    /**
+     * @param $longitude
+     * @param $latitude
+     * @dataProvider dataProviderWrongCoordinates
+     */
+    public function testCommandNotRunsWithWrongCoordinates($longitude, $latitude)
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Wrong coordinates');
+
+        Artisan::call('beers:find', [
+            '--lat' => $longitude,
+            '--long' => $latitude,
+        ]);
+    }
+
+    /**
+     * @param $longitude
+     * @param $latitude
+     * @dataProvider dataProviderNoBreweriesAreas
+     */
+    public function testCommandStopsIfNoBreweriesInGiveArea($longitude, $latitude)
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("No breweries in this area.");
+
+        Artisan::call('beers:find', [
+            '--lat' => $longitude,
+            '--long' => $latitude,
+        ]);
+    }
+
+    public function dataProviderWrongCoordinates()
+    {
+        return [
+            [
+                -181, 10
+            ],
+            [
+                181, 10
+            ],
+            [
+                10, -91
+            ],
+            [
+                10, 91
+            ]
+        ];
+    }
+
+    public function dataProviderNoBreweriesAreas()
+    {
+        return [
+            [
+                50, 100
+            ],
+            [
+                50, 150
+            ]
+        ];
     }
 }
